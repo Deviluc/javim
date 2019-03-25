b466b469be6d639580912ff28de1937224af9df8
deviluc <deviluc@googlemail.com>
2019-03-25 07:46:13 +0100

    Started adding tree-sitter

--- plugin/javim.vim
+++ plugin/javim.vim
@@ -54,6 +54,50 @@ javim.vim_quit()
 EOF
 endfunction
 
+function! javim#processTextChanged()
+python3 << EOF
+start_row, start_col = vim.eval('getpos("\'[")[1:2]')
+end_row, end_col = vim.eval('getpos("\']")[1:2]')
+#if start_row != end_row or start_col != end_col:
+print("Changed:", start_row, start_col, end_row, end_col)
+EOF
+endfunction
+
+function! javim#processTextChangedI()
+let text = @.
+python3 << EOF
+start_row, start_col = vim.eval('getpos("\'[")[1:2]')
+end_row, end_col = vim.eval('getpos("\']")[1:2]')
+#if start_row != end_row or start_col != end_col:
+
+text = vim.eval('text')
+print("ChangedI:", start_row, start_col, end_row, end_col, "Text:", text)
+EOF
+endfunction
+
+function! javim#processTextChangedP()
+python3 << EOF
+start_row, start_col = vim.eval('getpos("'[")[1:2]')
+end_row, end_col = vim.eval('getpos("']")[1:2]')
+#if start_row != end_row or start_col != end_col:
+text = vim.eval('@".')
+print("ChangedP:", start_row, start_col, end_row, end_col, "Text:", text)
+EOF
+endfunction
+
+
+function! javim#processTextYank()
+python3 << EOF
+event = vim.eval("v:event")
+if event['operator'] in ["d", "D"]:
+    lines = event['regcontents']
+    deleted = vim.eval('@"')
+    start_row, start_col = vim.eval('getpos("\'[")[1:2]')
+    offset = vim.eval("line2byte(line(\"'[\"))") + start_col
+    print(start_row, start_col, event, "Deleted: ", deleted)
+EOF
+endfunction
+
 :command! -nargs=1 -complete=dir ProjectImport call javim#projectImport(<f-args>)
 
 augroup javim
@@ -61,6 +105,10 @@ augroup javim
     autocmd BufEnter * :call javim#bufEnter(expand("<abuf>"))
     autocmd BufDelete * :call javim#bufDelete(expand("<abuf>"))
     autocmd VimLeave * call javim#vimQuit()
+    autocmd TextChanged * :call javim#processTextChanged()
+    autocmd TextChangedI * :call javim#processTextChangedI()
+    autocmd TextChangedP * :call javim#processTextChangedP()
+    autocmd TextYankPost * :call javim#processTextYank()
 augroup END
 
 :call javim#init()
--- plugin/javim/model.py
+++ plugin/javim/model.py
@@ -1,18 +1,83 @@
+from enumeration import Enum
 from javalang import tokenizer, parser
+
+from tree_sitter_api import Parser, InputEdit, Point
+from tree_sitter_java import tree_sitter_java
+
+class EditOperation(Enum):
+
+    INSERT = 0
+    REPLACE = 1
+    DELETE = 2
 
 class SourceFile():
 
-    def __init__(self, path, buffer):
+    def __init__(self, vim, path, buffer):
+        self.vim = vim
         self.path = path
         self.buffer = buffer
         self.line_count = len(self.buffer)
-        self.tokens = tokenizer.tokenize(self.buffer[:])
-        self.ast = parser.parse(self.tokens)
+        self.parser = Parser()
+        self.parser.set_language(tree_sitter_java())
+        self.tree = parser.parse(self.buffer[:])
 
-    def compute_change(self, vim, start, end):
+    def process_delete(self, start_row, start_col, start_byte, text):
+        edit = InputEdit()
+        edit.start_byte = start_byte
+        edit.old_end_byte = start_byte + len(text)
+        edit.new_end_byte = start_byte
+        edit.start_point = Point()
+        edit.start_point.row = start_row
+        edit.start_point.column = start_col
+        
+        line_offset = text.count("\n")
+        col_offset = len(text)
+        if line_offset:
+            last_line = text[text.rindex("\n")+1:]
+            col_offset = len(last_line)
+        
+        edit.old_end_point = Point()
+        edit.old_end_point.row = start_row + line_offset
+        if line_offset:
+            edit.old_end_point.column = col_offset
+        else:
+            edit.old_end_point.column = start_col + col_offset
+
+        edit.new_end_point = edit.start_point
+
+        self.tree.edit(edit)
+        self.tree = self.parser.parse_string(self.buffer[:], self.tree)
+
+    def process_insert(self, start_row, start_col, start_byte, text):
+        edit = InputEdit()
+        edit.start_byte = start_byte
+        edit.old_end_byte = start_byte
+        edit.new_end_byte = start_byte + len(text)
+
+        edit.start_point = Point()
+        edit.start_point.row = start_row
+        edit.start_point.column = start_col
+        
+        edit.old_end_point = edit.start_point
+
+        edit.new_end_point = Point()
+
+        line_offset = text.count("\n")
+        if line_offset:
+            last_line = text[text.rindex("\n")+1:]
+            edit.new_end_point.row = start_row + line_offset
+            edit.new_end_point.column = len(last_line)
+        else:
+            edit.new_end_point.row = start_row
+            edit.new_end_point.column = start_col + len(text)
+
+        self.tree.edit(edit)
+        self.tree = self.parser.parse_string(self.buffer[:], self.tree)
+
+
+    def compute_change(self, start_row, start_col):
         line_count = len(self.buffer)
-        start_row, start_col = start
-        end_row, end_col = end
+        edit = InputEdit()
         if line_count > self.line_count:
             # TODO: Handle inserted lines
             pass
@@ -22,3 +87,4 @@ class SourceFile():
         else:
             # TODO: handle changed lines
             pass
+        self.line_count = line_count
